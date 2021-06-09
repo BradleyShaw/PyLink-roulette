ba0e45e8425da16e9df269b93796671768a74c64
Bradley Shaw <bradley@shaw.ovh>
2021-06-09 23:17:43 +0100

    Create roulette.py

---
+++ roulette.py
@@ -0,0 +1,23 @@
+"""
+roulette.py: /KILL roulette
+"""
+import random
+
+from pylinkirc import utils
+
+
+def hook_privmsg(irc, source, command, args):
+    channel = args['target']
+    text = args['text']
+    try:
+        user = irc.users[source]
+    except KeyError:
+        return
+
+    if channel.lower() == '#roulette' and text.startswith('!roulette'):
+        if random.randint(1, 6) == 1:
+            irc.kill(irc.pseudoclient.uid, source, '\x02*BANG*\x02')
+        else:
+            irc.msg(channel, f'{user.nick}: *CLICK*')
+
+utils.add_hook(hook_privmsg, 'PRIVMSG')
